84cc546d0879edee0ed45851da95d0a728e33070
SontyanaNandini <153824304+SontyanaNandini@users.noreply.github.com>
2024-12-16 11:14:05 +0530

    Update settings.py

--- aws/settings.py
+++ aws/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ax2wspcyu@6g)h@^9cz3+@cn@r@fbpny@q1tx2251e=-%)zdki
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['43.204.110.115','localhost',0.0.0.0]
+ALLOWED_HOSTS = ['43.204.110.115','localhost','0.0.0.0']
 
 
 # Application definition
